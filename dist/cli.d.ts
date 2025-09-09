46afd07e635443606e7e586c6641c045fc289480
yxk <codewindow@icloud.com>
2025-09-09 16:35:15 +0800

    忽略dist

--- dist/cli.d.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-/**
- * CLI 执行入口
- */
-export {};
-//# sourceMappingURL=cli.d.ts.map
--- dist/types/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * 核心数据结构定义
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=index.js.map
